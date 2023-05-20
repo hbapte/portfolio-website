3128384b714fdc050cfb50eb1c06fd3e2bba031f
hbapte <ijbapte@gmail.com>
2023-05-20 09:20:07 +0200

    emailjs

--- script/script.js
+++ script/script.js
@@ -27,6 +27,41 @@ function showLoader() {
 
 
 
+
+  const client = new EmailJS("J6Lgb71e3Yjbw7vhN", "T_GBs1Zf33wMjGxgHUMKy");
+
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const name = document.querySelector("input[name=name]").value;
+  const email = document.querySelector("input[name=email]").value;
+  const message = document.querySelector("textarea[name=message]").value;
+
+  client.send({
+    to: "ijbapte@gmail.com",
+    from: "ijbapte@gmail.com.com",
+    subject: "This is a test email from " + name,
+    text: message,
+  });
+
+  // Add a success message
+  document.getElementById("success-message").textContent = "Email sent successfully!";
+  document.getElementById("success-message").style.display = "block";
+
+  // Clear the form
+  document.querySelector("input[name=name]").value = "";
+  document.querySelector("input[name=email]").value = "";
+  document.querySelector("textarea[name=message]").value = "";
+});
+
+// Add an error message
+client.on("error", (error) => {
+  document.getElementById("error-message").textContent = error.message;
+  document.getElementById("error-message").style.display = "block";
+});
+
+
+
 // var typed = new Typed('.typed', {
 //     strings: ['Frontend Developer', 'Blogger'],
 //     typeSpeed: 200,
