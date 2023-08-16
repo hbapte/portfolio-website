1f7463cb514ce055125e563307c66c246957e9ac
hbapte <ijbapte@gmail.com>
2023-08-16 07:55:27 +0200

    add loader

--- script/script.js
+++ script/script.js
@@ -1,29 +1,29 @@
 // ======= Loader ====== 
 
 // Function to show the loader
-// function showLoader() {
-//   document.getElementById("loader").style.display = "block";
-//   document.querySelectorAll("*").forEach((el) => (el.style.opacity = 0.6));
-// }
+function showLoader() {
+  document.getElementById("loader").style.display = "block";
+  document.querySelectorAll("*").forEach((el) => (el.style.opacity = 0.6));
+}
 
 // Function to hide the loader
-// function hideLoader() {
-//   document.getElementById("loader").style.display = "none";
-//   document.querySelectorAll("*").forEach((el) => (el.style.opacity = 1));
-// }
+function hideLoader() {
+  document.getElementById("loader").style.display = "none";
+  document.querySelectorAll("*").forEach((el) => (el.style.opacity = 1));
+}
 
-// // Event listener when the page finishes loading
-// window.addEventListener("load", function () {
-//   hideLoader(); // Hide the loader when the page finishes loading
-// });
+// Event listener when the page finishes loading
+window.addEventListener("load", function () {
+  hideLoader(); // Hide the loader when the page finishes loading
+});
 
-// // Event listener for when the website starts loading
-// window.addEventListener("beforeunload", function () {
-//   if (document.getElementById("loader").style.display === "block") {
-//     return; // Cancel the event and allow the user to leave the page
-//   }
-//   showLoader(); // Show the loader when the website starts loading
-// });
+// Event listener for when the website starts loading
+window.addEventListener("beforeunload", function () {
+  if (document.getElementById("loader").style.display === "block") {
+    return; // Cancel the event and allow the user to leave the page
+  }
+  showLoader(); // Show the loader when the website starts loading
+});
 
 // ====== Emailjs ===== 
 
